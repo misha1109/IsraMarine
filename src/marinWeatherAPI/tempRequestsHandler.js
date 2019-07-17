cded0f6e0ed2174453d9b71b21847f5f267f09d5
misha1109 <misha110990@gmail.com>
2019-07-18 00:13:47 +0300

    forecast added

--- src/marinWeatherAPI/tempRequestsHandler.js
+++ src/marinWeatherAPI/tempRequestsHandler.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const urlPath = 'http://api.worldweatheronline.com/premium/v1/marine.ashx/?key=57c12832c96a4554b2e94309191407&'
+const urlPath = 'https://api.worldweatheronline.com/premium/v1/marine.ashx/?key=57c12832c96a4554b2e94309191407&'
 
 export default async function getData( coordinates){
     let query = `q=${coordinates[0]},${coordinates[1]}`
